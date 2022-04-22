2c02873bf5e27e285a7cb9947636e939ffd4020d
t29mato <tomoya.matou@gmail.com>
2022-04-22 17:24:57 +0900

    add sendmail

--- main.sh
+++ main.sh
@@ -4,6 +4,9 @@ DB_HOST=$1
 DB_PORT=$2
 DB_NAME=$3
 BACKUP_BASE_DIR=$4
+MAIL_FROM=$5
+MAIL_TO=$6
+MAIL_CC=$7
 BACKUP_DIR=$BACKUP_BASE_DIR/backup/$DB_NAME/
 DUMP_DIR=$BACKUP_DIR/dump
 BACKUP_FILE=$BACKUP_DIR/$DB_NAME_`date +"%Y%m%d-%H%M%S"`.tar
@@ -25,6 +28,21 @@ fi
 
 if [ -z $BACKUP_BASE_DIR ]; then
     echo 'require backup base directory'
+    exit
+fi
+
+if [ -z $MAIL_FROM ]; then
+    echo 'require mail from'
+    exit
+fi
+
+if [ -z $MAIL_TO ]; then
+    echo 'require mail to'
+    exit
+fi
+
+if [ -z $MAIL_CC ]; then
+    echo 'require mail cc'
     exit
 fi
 
@@ -58,6 +76,16 @@ function afterprocess() {
     return 0
 }
 
+function complete() {
+    BACKUP_FILE=./main.sh
+    echo $BACKUP_FILE >> ./body.txt
+    echo $(ls -lah $BACKUP_FILE) >> ./body.txt
+    sed -e "s/<FROM>/$MAIL_FROM/" -e "s/<TO>/$MAIL_TO/" -e "s/<CC>/$MAIL_CC/" ./templates/success.txt | cat - ./body.txt | sendmail -i -t
+    echo 'sent a mail'
+    rm -f ./body.txt
+    return 0
+}
+
 echo 'preprocess'
 preprocess
 echo 'main_mongodump'
@@ -66,4 +94,5 @@ echo 'compress'
 compress
 echo 'afterprocess'
 afterprocess
+complete
 echo 'complete'
